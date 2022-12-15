cdeee85c68807981ae7a941b73305617a9ecb350
johnkim1216 <johnmjkim1216@gmail.com>
2022-12-15 14:28:34 +1100

    1.1 done

--- part1/my-app/src/App.js
+++ part1/my-app/src/App.js
@@ -1,3 +1,29 @@
+const Header = (header_props) => {
+  return (
+    <div>
+      <h1>{header_props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (content_props) => {
+  return (
+    <div>
+      <p>{content_props.part} {content_props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (total_props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {total_props.exercises1 + total_props.exercises2 + total_props.exercises3}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +35,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
