18304b97fdda2ca04081cc8cbbd28a0796dac3e9
johnkim1216 <johnmjkim1216@gmail.com>
2022-12-15 14:38:02 +1100

    1.2 done

--- part1/my-app/src/App.js
+++ part1/my-app/src/App.js
@@ -6,10 +6,22 @@ const Header = (header_props) => {
   )
 }
 
+const Part = (part_props) => {
+  return (
+    <div>
+      <p>
+        {part_props.part} {part_props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const Content = (content_props) => {
   return (
     <div>
-      <p>{content_props.part} {content_props.exercises}</p>
+      <Part part={content_props.part1} exercises={content_props.exercises1} />
+      <Part part={content_props.part2} exercises={content_props.exercises2} />
+      <Part part={content_props.part3} exercises={content_props.exercises3} />
     </div>
   )
 }
@@ -36,9 +48,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
