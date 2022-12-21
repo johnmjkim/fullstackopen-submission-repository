8a9c914496e0391a0c33656c9a65a047c16953b6
johnkim1216 <johnmjkim1216@gmail.com>
2022-12-21 15:41:38 +1100

    done 1.7 unicafe step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,55 @@
 import { useState } from 'react'
 
+const Display = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+)
+
+const DisplayPercent = (props) => (
+  <div>
+    {props.text} {props.value*100}%
+  </div>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let sum = good + neutral + bad
 
   return (
     <div>
-      code here
+      <h1>
+        give feedback
+      </h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <h1>
+        statistics
+      </h1>
+      <p>
+        <Display value={good} text={"good"}></Display>
+        <Display value={neutral} text={"neutral"}></Display>
+        <Display value={bad} text={"bad"}></Display>
+        <Display value={sum} text={"all"}></Display>
+        <Display value={(good - bad)/sum} text={"average"}></Display>
+        <DisplayPercent value={good/sum} text={"positive"}></DisplayPercent>
+      </p>
     </div>
   )
 }
